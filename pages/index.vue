b6b939ddcb91c096623b6c4df9c5f592ee3c88ad
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2023-01-10 23:39:10 +0200

    /

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,26 @@
 <script setup></script>
 <template>
   <div class="container">
-    <header class="bg-sky-400 h-96"></header>
-    <main></main>
+    <header
+      class="w-full h-screen bg-cover flex justify-start items-center flex-col gap-10 pt-28"
+    >
+      <h2
+        class="text-white text-center text-5xl font-black font-serif uppercase tracking-widest leading-tight"
+      >
+        we are creatives
+      </h2>
+      <img src="~/assets/images/icon-arrow-down.svg" alt="arrow down" />
+    </header>
+    <main class="h-80 bg-cover"></main>
     <footer></footer>
   </div>
 </template>
 
-<style></style>
+<style>
+header {
+  background: url("~/assets/images/mobile/image-header.jpg");
+}
+main {
+  background: url("~/assets/images/mobile/image-transform.jpg");
+}
+</style>
