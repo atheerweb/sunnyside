69f26d601b21cb1eee343d6e01daa5d4b97a4110
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2023-01-10 16:37:15 +0200

    /

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <nav class="p-5 bg-transparent fixed flex justify-between w-full">
+      <img src="~/assets/images/logo.svg" alt="logo" />
+      <img src="~/assets/images/icon-hamburger.svg" alt="mhamburger menu" />
+    </nav>
+    <slot />
+  </div>
+</template>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+  <div class="container">
+    <header class="bg-sky-400 h-96"></header>
+    <main></main>
+    <footer></footer>
+  </div>
+</template>
+
+<style></style>
